4cd617ed6875520d9785565150d2e99f5e164d87
Aleksandar Zgonjan <aleksandar.zgonjan@medisante-group.com>
2021-04-28 14:54:51 +0200

    Add sequence, languages_id_seq

--- database/migrations/20210207000003_create_languages_table.up.sql
+++ database/migrations/20210207000003_create_languages_table.up.sql
@@ -1,3 +1,14 @@
+-- public.languages_id_seq definition
+
+-- DROP SEQUENCE public.languages_id_seq;
+
+CREATE SEQUENCE public.languages_id_seq
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 9223372036854775807
+	CACHE 1
+	NO CYCLE;
+
 -- Table: public.languages
 
 -- DROP TABLE public.languages;
