88fd84493d268d3239383b4a9935edd73b708cee
Aleksandar Zgonjan <aleksandar.zgonjan@medisante-group.com>
2021-04-28 14:46:35 +0200

    Add missing sequence, countries_id_seq

--- database/migrations/20210207000001_create_countries_table.up.sql
+++ database/migrations/20210207000001_create_countries_table.up.sql
@@ -1,3 +1,14 @@
+-- public.countries_id_seq definition
+
+-- DROP SEQUENCE public.countries_id_seq;
+
+CREATE SEQUENCE public.countries_id_seq
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 9223372036854775807
+	CACHE 1
+	NO CYCLE;
+
 -- Table: public.countries
 
 -- DROP TABLE public.countries;
