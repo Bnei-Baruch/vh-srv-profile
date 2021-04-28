7625a224c86a0063ed25d431eb4701c05f2b1fec
Aleksandar Zgonjan <aleksandar.zgonjan@medisante-group.com>
2021-04-28 15:01:06 +0200

    Add sequence, users_id_seq

--- database/migrations/20210207000006_create_users_table.up.sql
+++ database/migrations/20210207000006_create_users_table.up.sql
@@ -1,3 +1,14 @@
+-- public.users_id_seq definition
+
+-- DROP SEQUENCE public.users_id_seq;
+
+CREATE SEQUENCE public.users_id_seq
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 9223372036854775807
+	CACHE 1
+	NO CYCLE;
+
 -- Table: public.users
 
 -- DROP TABLE public.users;
