3cc74a4a5796f34c54c59f1243d8913b7fc5c4ed
Shirley Leu <shirley.w.leu@gmail.com>
2021-05-09 13:02:03 +0200

    Begin db create profile

--- db_user.go
+++ db_user.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/jackc/pgx/v4"
+	"github.com/jackc/pgx/v4/pgxpool"
+	uuid "github.com/satori/go.uuid"
+)
+
 type user struct {
 	keycloakID          string
 	firstNameLatin      *string
@@ -60,3 +70,114 @@ const (
 	whatsApp = "WhatsApp"
 	telegram = "Telegram"
 )
+
+type pgProfileDB struct {
+	*pgxpool.Pool
+}
+
+func newPgProfileDB(ctx context.Context) (*pgProfileDB, error) {
+	pool, err := pgxpool.Connect(ctx, os.Getenv("DATABASE_URL"))
+	if err != nil {
+		return nil, fmt.Errorf("unable to connect to database: %w", err)
+	}
+
+	return &pgProfileDB{pool}, nil
+}
+
+func (db *pgProfileDB) CreateUser(ctx context.Context, user user) error {
+	tx, err := db.Begin(ctx)
+	if err != nil {
+		return err
+	}
+	defer func() { _ = tx.Rollback(ctx) }()
+
+	var userID uuid.UUID
+	err = db.QueryRow(ctx, `
+	INSERT INTO users (keycloak_id,
+                   first_name_latin,
+                   first_name_vernacular,
+                   last_name_latin,
+                   last_name_vernacular,
+                   street_address,
+                   country,
+                   state_region,
+                   postal_code,
+                   city,
+                   gender,
+                   marital_status,
+                   date_of_birth,
+                   primary_email,
+                   alternate_email_1,
+                   alternate_email_2,
+                   first_language,
+                   other_language_1,
+                   other_language_2,
+                   other_language_3,
+                   other_language_4,
+                   listening_language,
+                   reading_language,
+                   email_language,
+                   study_start_year,
+                   study_framework,
+                   has_ten_group,
+                   wants_ten_group,
+                   name_of_ten_group)
+	VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24,
+        $25, $26, $27, $28, $29)
+	RETURNING user_id)`,
+		user.keycloakID,
+		user.firstNameLatin,
+		user.firstNameVernacular,
+		user.lastNameLatin,
+		user.lastNameVernacular,
+		user.address.streetAddress,
+		user.address.country,
+		user.address.stateOrRegion,
+		user.address.postalCode,
+		user.address.city,
+		user.gender,
+		user.maritalStatus,
+		user.dateOfBirth,
+		user.emails.primary,
+		user.emails.alternate1,
+		user.emails.alternate2,
+		user.languages.first,
+		user.languages.other1,
+		user.languages.other2,
+		user.languages.other3,
+		user.languages.other4,
+		user.languages.listening,
+		user.languages.reading,
+		user.languages.email,
+		user.studyStartYear,
+		user.studyFramework,
+		user.ten.hasGroup,
+		user.ten.wantsGroup,
+		user.ten.nameOfGroup).Scan(&userID)
+
+	if user.phones.mobileNumber != nil {
+		if err := insertPhone(tx, userID, *user.phones.mobileNumber, mobile); err != nil {
+			return err
+		}
+	}
+
+	if user.phones.whatsAppNumber != nil {
+		if err := insertPhone(tx, userID, *user.phones.whatsAppNumber, whatsApp); err != nil {
+			return err
+		}
+	}
+
+	if user.phones.telegramNumber != nil {
+		if err := insertPhone(tx, userID, *user.phones.telegramNumber, telegram); err != nil {
+			return err
+		}
+	}
+
+	return tx.Commit(ctx)
+}
+
+func insertPhone(tx pgx.Tx, userID uuid.UUID, number string, phoneType string) error {
+	_, err := tx.Exec(context.Background(), `INSERT INTO phone_numbers (user_id, phone_number, type) VALUES ($1, $2, $3)`,
+		userID, number, phoneType)
+	return err
+}
