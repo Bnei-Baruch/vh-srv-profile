ccf6380b7bebb9cbba4e886ff10151b6c175d795
Shirley Leu <shirley.w.leu@gmail.com>
2021-05-09 12:56:25 +0200

    Update profile request

--- go.mod
+++ go.mod
@@ -8,5 +8,6 @@ require (
 	github.com/gin-gonic/gin v1.6.3
 	github.com/go-pg/pg/v10 v10.7.3
 	github.com/joho/godotenv v1.3.0
+	github.com/stretchr/testify v1.6.1
 	golang.org/x/crypto v0.0.0-20201117144127-c1f2f97bffc9
 )
--- handler_create.go
+++ handler_create.go
@@ -1,78 +1,36 @@
 package main
 
-import (
-	"net/http"
-	"time"
-
-	"github.com/gin-gonic/gin"
-	"gitlab.bbdev.team/vh/vh-srv-profile/models"
-)
-
-//ProfileCreateRequest
-type ProfileCreateRequest struct {
-	FirstName string `json:"first_name" binding:"required"`
-	LastName  string `json:"last_name" binding:"required"`
-	Phone     string `json:"phone" binding:"required"`
-	Email     string `json:"email" binding:"required"`
-	Country   int    `json:"country" binding:"required"`
-	Language  int    `json:"language" binding:"required"`
-	BirthDate string `json:"birthdate" binding:"required"`
-	Gender    int    `json:"gender" binding:"required"`
-}
-
-//ProfileCreate create profile
-func ProfileCreate(c *gin.Context) {
-
-	var request ProfileCreateRequest
-
-	if err := c.Bind(&request); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": err.Error(),
-		})
-		return
-	}
-
-	user := &models.User{
-		FirstName: request.FirstName,
-		LastName:  request.LastName,
-		Phone:     request.Phone,
-		Email:     request.Email,
-		Active:    true,
-		Gender:    request.Gender,
-		Country:   request.Country,
-		Language:  request.Country,
-	}
-
-	birthdate, err := time.Parse("2006-01-02", request.BirthDate)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": err.Error(),
-		})
-
-		return
-	}
-
-	user.BirthDate = birthdate
-
-	errors, ok := models.ValidateUser(user, true)
-
-	if !ok {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": errors,
-		})
-
-		return
-	}
-
-	err = models.InsertUser(user)
-
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": err.Error(),
-		})
-		return
-	}
-
-	c.Status(http.StatusOK)
-
+type createUser struct {
+	KeycloakID          string  `json:"keycloak_id" binding:"required"`
+	FirstNameLatin      string  `json:"first_name_latin" binding:"required"`
+	FirstNameVernacular *string `json:"first_name_vernacular"`
+	LastNameLatin       string  `json:"last_name_latin" binding:"required"`
+	LastNameVernacular  *string `json:"last_name_vernacular"`
+	StreetAddress       *string `json:"street_address"`
+	Country             *string `json:"country"`
+	StateOrRegion       *string `json:"state_region"`
+	PostalCode          *string `json:"postal_code"`
+	City                *string `json:"city"`
+	Gender              string  `json:"gender" binding:"required"`
+	MaritalStatus       string  `json:"marital_status" binding:"required"`
+	DateOfBirth         *string `json:"date_of_birth"`
+	PrimaryEmail        string  `json:"primary_email" binding:"required"`
+	AlternateEmail1     *string `json:"alternate_email_1"`
+	AlternateEmail2     *string `json:"alternate_email_2"`
+	MobileNumber        string  `json:"mobile_number" binding:"required"`
+	WhatsAppNumber      *string `json:"whats_app_number"`
+	TelegramNumber      *string `json:"telegram_number"`
+	FirstLanguage       string  `json:"first_language" binding:"required"`
+	OtherLanguage1      *string `json:"other_language_1"`
+	OtherLanguage2      *string `json:"other_language_2"`
+	OtherLanguage3      *string `json:"other_language_3"`
+	OtherLanguage4      *string `json:"other_language_4"`
+	ListeningLanguage   string  `json:"listening_language" binding:"required"`
+	ReadingLanguage     string  `json:"reading_language" binding:"required"`
+	EmailLanguage       string  `json:"email_language" binding:"required"`
+	StudyStartYear      int     `json:"study_start_year" binding:"required"`
+	StudyFramework      string  `json:"study_framework" binding:"required"`
+	HasGroup            bool    `json:"has_ten_group" binding:"required"`
+	WantsGroup          *bool   `json:"wants_ten_group"`
+	NameOfGroup         *bool   `json:"name_ten_group"`
 }
