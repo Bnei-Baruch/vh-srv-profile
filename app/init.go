644e62892e4a8815765112e8d19325e41d8b4630
Shirley Leu <shirley.w.leu@gmail.com>
2021-05-13 16:53:26 +0200

    Remove config package

--- app/init.go
+++ app/init.go
@@ -1,32 +1,4 @@
 package app
 
-import (
-	"encoding/json"
-	"log"
-
-	"gitlab.bbdev.team/vh/vh-srv-profile/config"
-)
-
-//Config app config
-var Config config.AppConfig
-
 //SystemMessages system text message
 var SystemMessages SystemMessagesType
-
-func init() {
-	if err := json.Unmarshal([]byte(`{
-    "data": {
-        "email_not_valid": "E-mail is not valid.",
-        "email_exists": "E-mail already exists.",
-        "phone_exists":"Phone already exists.",
-        "password_min_length_error": "Minimum password length 6 characters.",
-        "password_max_length_error": "Maximum password length 32 characters.",
-        "wrong_current_password": "wrong  current password",
-        "wrong_password_confirmation": "The password confirmation does not match.",
-        "access_denied": "Access denied",
-        "account_blocked": "Account blocked"
-    }
-}`), &SystemMessages); err != nil {
-		log.Fatal(err)
-	}
-}
