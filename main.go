ee95aa6447309d1274da8f5424dcbeb3b4d3ae24
Yasha <3-yasha@users.noreply.gitlab.bbdev.team>
2021-07-31 05:20:03 +0200

    removed cors again

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 	"os"
 	"time"
 
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
@@ -64,9 +63,7 @@ type appHandlers struct {
 
 func initApp(handlers appHandlers) *gin.Engine {
 	app := gin.Default()
-	if os.Getenv("CORSACTIVE") == "true" {
-		app.Use(cors.Default())
-	}
+	//app.Use(cors.Default())
 
 	app.POST("/v1/profile", handlers.create)
 	app.GET("/v1/profile/:keycloak_id", handlers.get)
