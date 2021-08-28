b38d7e8ea287fe3c9e06f76a1ac1f441bb79cd47
Yasha <3-yasha@users.noreply.gitlab.bbdev.team>
2021-08-28 13:48:42 +0200

    new table to store the info

---
+++ db/migration_01.sql
@@ -0,0 +1,12 @@
+BEGIN;
+
+CREATE TABLE status (
+    membership bool default false,
+    membership_type text default 'inactive',
+    ticket bool default false,
+    convention bool default false,
+    galaxy bool default false,
+    user_id      uuid NOT NULL REFERENCES users (user_id),
+)
+
+COMMIT;
