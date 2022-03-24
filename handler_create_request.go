64732e11a58923cd804a03ac978bf8d89f404715
Abhinav <abhinavshm95@gmail.com>
2022-03-24 15:50:04 +0530

    feat: Create Request

---
+++ handler_create_request.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type createRequestStorage interface {
+	createRequest(ctx context.Context, request newRequest) error
+}
+
+func (p *profileManager) createRequest(c *gin.Context) {
+	var request newRequest
+
+	if err := c.ShouldBind(&request); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		_ = c.Error(err)
+		return
+	}
+
+	if request.KeycloakId == nil || request.RequestName == nil || request.Status == nil {
+		err := fmt.Errorf("missing a required field in provided request: %#v", request)
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		_ = c.Error(err)
+		return
+	}
+
+	if *request.Status != "REQUESTED" && *request.Status != "APPROVED" && *request.Status != "DENIED" {
+		err := fmt.Errorf("invalid status: %s", *request.Status)
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		_ = c.Error(err)
+		return
+	}
+
+	if err := p.requestCreator.createRequest(c.Request.Context(), request); err != nil {
+		c.Status(http.StatusInternalServerError)
+		_ = c.Error(fmt.Errorf("error while creating request %q: %w", *request.KeycloakId, err))
+		return
+	}
+
+	c.Status(http.StatusCreated)
+}
