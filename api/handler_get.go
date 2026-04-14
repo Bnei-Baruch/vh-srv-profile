f5f5d53f28958540a841169367d983b187b2883f
bbfsdev <bbfsdev@gmail.com>
2026-04-14 01:47:12 +0000

    Don't hide any non set field in profile.

--- api/handler_get.go
+++ api/handler_get.go
@@ -34,38 +34,38 @@ type userResponse struct {
 	Status              status      `json:"status"`
 	MembershipActive    *bool       `json:"membership_active"`
 	MembershipType      *string     `json:"membership_type"`
-	FirstNameLatin      *string     `json:"first_name_latin,omitempty"`
-	FirstNameVernacular *string     `json:"first_name_vernacular" `
-	LastNameLatin       *string     `json:"last_name_latin,omitempty"`
-	LastNameVernacular  *string     `json:"last_name_vernacular"  `
-	StreetAddress       *string     `json:"street_address,omitempty"`
-	Country             *string     `json:"country,omitempty"`
-	StateOrRegion       *string     `json:"state_region,omitempty"`
-	PostalCode          *string     `json:"postal_code,omitempty"`
-	City                *string     `json:"city,omitempty"`
-	Gender              *string     `json:"gender,omitempty"`
+	FirstNameLatin      *string     `json:"first_name_latin"`
+	FirstNameVernacular *string     `json:"first_name_vernacular"`
+	LastNameLatin       *string     `json:"last_name_latin"`
+	LastNameVernacular  *string     `json:"last_name_vernacular"`
+	StreetAddress       *string     `json:"street_address"`
+	Country             *string     `json:"country"`
+	StateOrRegion       *string     `json:"state_region"`
+	PostalCode          *string     `json:"postal_code"`
+	City                *string     `json:"city"`
+	Gender              *string     `json:"gender"`
 	MaritalStatus       null.String `json:"marital_status"`
-	SpouseKeycloakID    *string     `json:"spouse_keycloak_id,omitempty"`
-	DateOfBirth         *string     `json:"date_of_birth,omitempty"`
-	PrimaryEmail        *string     `json:"primary_email" `
-	AlternateEmail1     *string     `json:"alternate_email_1,omitempty"`
-	AlternateEmail2     *string     `json:"alternate_email_2,omitempty"`
-	MobileNumber        *string     `json:"mobile_number,omitempty"`
-	WhatsAppNumber      *string     `json:"whats_app_number,omitempty"`
-	TelegramNumber      *string     `json:"telegram_number,omitempty"`
-	FirstLanguage       *string     `json:"first_language,omitempty"`
-	OtherLanguage1      *string     `json:"other_language_1,omitempty"`
-	OtherLanguage2      *string     `json:"other_language_2,omitempty"`
-	OtherLanguage3      *string     `json:"other_language_3,omitempty"`
-	OtherLanguage4      *string     `json:"other_language_4,omitempty"`
-	ListeningLanguage   *string     `json:"listening_language,omitempty"`
-	ReadingLanguage     *string     `json:"reading_language,omitempty"`
-	EmailLanguage       *string     `json:"email_language,omitempty"`
-	StudyStartYear      *int        `json:"study_start_year,omitempty"`
-	StudyFramework      *string     `json:"study_framework,omitempty"`
-	HasGroup            *bool       `json:"has_ten_group,omitempty"`
-	WantsGroup          *bool       `json:"wants_ten_group,omitempty"`
-	NameOfGroup         *string     `json:"name_ten_group,omitempty"`
+	SpouseKeycloakID    *string     `json:"spouse_keycloak_id"`
+	DateOfBirth         *string     `json:"date_of_birth"`
+	PrimaryEmail        *string     `json:"primary_email"`
+	AlternateEmail1     *string     `json:"alternate_email_1"`
+	AlternateEmail2     *string     `json:"alternate_email_2"`
+	MobileNumber        *string     `json:"mobile_number"`
+	WhatsAppNumber      *string     `json:"whats_app_number"`
+	TelegramNumber      *string     `json:"telegram_number"`
+	FirstLanguage       *string     `json:"first_language"`
+	OtherLanguage1      *string     `json:"other_language_1"`
+	OtherLanguage2      *string     `json:"other_language_2"`
+	OtherLanguage3      *string     `json:"other_language_3"`
+	OtherLanguage4      *string     `json:"other_language_4"`
+	ListeningLanguage   *string     `json:"listening_language"`
+	ReadingLanguage     *string     `json:"reading_language"`
+	EmailLanguage       *string     `json:"email_language"`
+	StudyStartYear      *int        `json:"study_start_year"`
+	StudyFramework      *string     `json:"study_framework"`
+	HasGroup            *bool       `json:"has_ten_group"`
+	WantsGroup          *bool       `json:"wants_ten_group"`
+	NameOfGroup         *string     `json:"name_ten_group"`
 }
 
 type ShortProfile struct {
